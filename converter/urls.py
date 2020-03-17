2e6328a6f8456b30c183a599d9151d2740c25c91
Iliyan Videv <videviliyan@gmail.com>
2020-03-17 10:43:38 +0200

    Added a url mapping for the currencies page

--- converter/urls.py
+++ converter/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'converter'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('currencies/', views.currencies, name='list_currencies'),
     path('convert/', views.convert, name='convert')
 ]
