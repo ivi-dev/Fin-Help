e80f630d9b5271321fde5536862eb82f18db004f
Iliyan Videv <videviliyan@gmail.com>
2020-03-17 13:45:47 +0200

    Removed an unused URL mapping

--- converter/urls.py
+++ converter/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = 'converter'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('currencies/', views.currencies, name='list_currencies'),
     path('convert/', views.convert, name='convert')
 ]
