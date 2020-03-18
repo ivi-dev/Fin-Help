b5e111b95b9e7d8efc2da2ca3ddbe22a5650811a
Iliyan Videv <videviliyan@gmail.com>
2020-03-18 16:01:25 +0200

    Refactored view code to minimize db queries

--- converter/views.py
+++ converter/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+import datetime
 
 from .models import Currency
+from .general import get_currency_data, \
+					 process_currency_data, \
+					 qs_find
 
 
 def index(request):
 	currencies = Currency.objects.all()
+	from_currency = currencies.first()
+	to_currency = from_currency
 	context = {
 		'currencies': currencies,
 		'amount': 1,
-		'from_currency': currencies[0],
-		'to_currency': currencies[0],
-		'rate': currencies[0].get_rate_to(currencies[0].code, 
+		'from_currency': from_currency,
+		'to_currency': to_currency,
+		'rate': from_currency.get_rate_to(from_currency.code, 
 							              precision=2),
-		'latest_rate_update': currencies[0].latest_rate_update,
+		'latest_rate_update': from_currency.latest_rate_update,
 		'conversion_result': 1
 	}
 	return render(request, 'converter/index.html', context)
@@ -30,6 +36,7 @@ def convert(request):
 	result = from_currency.convert_to(to_code, amount, precision=2)
 	return HttpResponse(json.dumps({'result': str(result), 
 							        'rate_info': {
+							        	'amount': amount,
 							        	'from_currency_name': from_currency.name,
 							        	'from_currency_symbol': from_currency.symbol,
 							        	'rate': str(rate),
@@ -37,5 +44,12 @@ def convert(request):
 							        	'to_currency_symbol': to_currency.symbol
 							        }}))
 
-def update_rates(request):
-	
+def update_currencies(request):
+	data = get_currency_data()
+	existing = Currency.objects.all()
+	new, updated = process_currency_data(data, existing)
+
+	Currency.objects.bulk_update(updated, ['name', 'code', 'per', 'rate'])
+	Currency.objects.bulk_create(new)
+
+	return HttpResponse('')
