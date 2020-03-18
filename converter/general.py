bed68176de8d7e421c120873cce43447029aefda
Iliyan Videv <videviliyan@gmail.com>
2020-03-18 21:09:41 +0200

    Moved general.py to utility

--- converter/general.py
+++
@@ -1,8 +0,0 @@
-RATES_URL = 'https://www.bnb.bg/Statistics/StExternalSector/StExchangeRates/StERForeignCurrencies/index.htm'
-RATES_ELEMENT_NAME = 'form'
-RATES_ELEMENT_ID = 'Exchange_Rate_Search'
-
-def qs_find(queryset, filter):
-	for item in queryset:
-		if getattr(item, filter[0]) == filter[1]:
-			return item
