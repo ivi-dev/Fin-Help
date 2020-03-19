facdd71a8b304829af189ecf363e10022896a4ad
Iliyan Videv <videviliyan@gmail.com>
2020-03-19 07:14:43 +0200

    Renamed the admin's currency list url

--- converter/urls.py
+++ converter/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('convert/', views.convert, name='convert'),
     path('update-currencies/', views.update_currencies, 
 	     name='update_currencies'),
-    path('admin/converter/currency/', views.update_currencies, 
+    path('admin/converter/currency/', views.view_admin_currencies_list, 
     	 name='admin-currencies-list'),
 ]
