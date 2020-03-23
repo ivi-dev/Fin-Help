cb9cf31ecf2c8cd23d2039f1e32406792fbc70cf
Iliyan Videv <videviliyan@gmail.com>
2020-03-23 17:51:02 +0200

    Changed most of the constants in constants.js to point to jQuery elements

--- converter/static/converter/js/constants.js
+++ converter/static/converter/js/constants.js
@@ -1,11 +1,15 @@
 const ALERT_BOX = $('#alert-box');
+
 const CONVERT_FORM = $('#convert-form');
-const AMOUNT_FIELD_SELECTOR = '#amount';
-const FROM_FIELD_SELECTOR = '#from';
-const TO_FIELD_SELECTOR = '#to';
-const CONVERT_BUTTON_SELECTOR = 'button#convert';
+const AMOUNT_FIELD = CONVERT_FORM.children('#amount');
+const FROM_FIELD = CONVERT_FORM.children('#from');
+const TO_FIELD = CONVERT_FORM.children('#to');
+const FLIP_BUTTON = CONVERT_FORM.children('button#flip');
+const CONVERT_BUTTON = CONVERT_FORM.children('button#convert');
+const CONVERT_BUTTON_ORIGINAL_TITLE = CONVERT_BUTTON.text();
 const CONVERT_BUTTON_BUSY_TITLE = 'КАЛКУЛИРАМ...';
-const CONVERT_BUTTON_ORIGINAL_TITLE = $(CONVERT_BUTTON_SELECTOR).text();
-const VIEW_CURRENCIES_BUTTON = $('#view-currencies');
+
 const CURRENCIES_LIST = $('#currencies-list');
-const RATES_URL = 'https://www.bnb.bg/Statistics/StExternalSector/StExchangeRates/StERForeignCurrencies/index.htm';
+const VIEW_CURRENCIES_BUTTON = $('#view-currencies');
+const RATES_URL = 'https://www.bnb.bg/Statistics/StExternalSector' +
+			      '/StExchangeRates/StERForeignCurrencies/index.htm';
