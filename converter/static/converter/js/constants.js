3d6c103e90d37c1024316ef9f6ad752479181b71
Iliyan Videv <videviliyan@gmail.com>
2020-03-24 22:30:09 +0200

    Added trimming of the CONVERT_BUTTON_ORIGINAL_TITLE's value

--- converter/static/converter/js/constants.js
+++ converter/static/converter/js/constants.js
@@ -6,7 +6,7 @@ const FROM_FIELD = CONVERT_FORM.children('#from');
 const TO_FIELD = CONVERT_FORM.children('#to');
 const FLIP_BUTTON = CONVERT_FORM.children('button#flip');
 const CONVERT_BUTTON = CONVERT_FORM.children('button#convert');
-const CONVERT_BUTTON_ORIGINAL_TITLE = CONVERT_BUTTON.text();
+const CONVERT_BUTTON_ORIGINAL_TITLE = CONVERT_BUTTON.text().trim();
 const CONVERT_BUTTON_BUSY_TITLE = 'КАЛКУЛИРАМ...';
 
 const CURRENCIES_LIST = $('#currencies-list');
